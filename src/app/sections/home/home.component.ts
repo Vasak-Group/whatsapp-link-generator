e0463b30286f87646377ce6bfb2822f51a64c58b
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-10-05 13:21:47 -0300

    Fix isNaN

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  text:String = "";
-  tel:Number = NaN;
+  text:string = "";
+  tel:number = NaN;
 
   constructor() { }
 
@@ -16,18 +16,18 @@ export class HomeComponent implements OnInit {
   }
 
   getNumber():string{
-    if (this.tel == NaN){
+    if (isNaN(this.tel)){
       return '';
     }else{
       return this.tel.toString();
     }
   }
 
-  getText():String{
+  getText():string{
     return this.text.split(' ').join('%20')
   }
 
-  generateLink():String{
+  generateLink():string{
     return(
       `https://wa.me/${this.getNumber()}?text=${this.getText()}`
     )
