a5bb8b300710e7489a3c6c6ecbee4d7963af2389
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-10-05 09:10:37 -0300

    Init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './partial/topbar/topbar.component';
+import { FooterComponent } from './partial/footer/footer.component';
+import { HomeComponent } from './sections/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
